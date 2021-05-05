907465457041417b0b7c23158fb801b3e07ee6a6
riyajkafar <riyajkafar@gmail.com>
2021-05-05 20:50:03 +0530

    Add

---
+++ app/src/main/java/com/example/gitapplication/editrecoad.java
@@ -0,0 +1,14 @@
+package com.example.gitapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class editrecoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editrecoad);
+    }
+}
